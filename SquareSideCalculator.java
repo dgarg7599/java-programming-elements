760a1ed5a76bb995062de949f5852a1337f1e4b2
dgarg7599 <gargdivyansh7599@gmail.com>
2025-02-28 23:02:45 +0530

    [Divyansh Garg] ADD: Calculating the side of square using its perimeter.

---
+++ SquareSideCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SquareSideCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the perimeter of the square: ");
+        double perimeter = input.nextDouble();
+        // Calculate side length using the formula: Side = Perimeter / 4
+        double side = perimeter / 4;
+        // Display the result
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+    }
+}
