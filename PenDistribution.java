b10279279ce4f633c609c9a2ccc0a08024b3d1b4
dgarg7599 <gargdivyansh7599@gmail.com>
2025-02-28 18:13:01 +0530

    [Divyansh Garg] ADD: Calculation of equal pen distribution and remaining pens.

---
+++ PenDistribution.java
@@ -0,0 +1,15 @@
+public class PenDistribution {
+    public static void main(String[] args) {
+        int totalPens = 14;
+        int totalStudents = 3;
+        // Calculate pens per student
+        int pensPerStudent = totalPens / totalStudents;
+	// Calculate remaining pens
+        int remainingPens = totalPens % totalStudents;
+        // Display the result
+        System.out.println(
+            "The Pen Per Student is " + pensPerStudent + 
+            " and the remaining pen not distributed is " + remainingPens
+        );
+    }
+}
