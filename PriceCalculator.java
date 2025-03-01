ffaf811be26ab642065c9e744941c72a979a826e
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-01 10:26:31 +0530

    [Divyansh Garg] ADD: Calculating the total purchase price.

--- PriceCalculator.java
+++ PriceCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PurchasePriceCalculator {
+public class PriceCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the unit price (INR): ");
