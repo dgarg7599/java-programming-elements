0bd204f180da6b489954a5fc6cd81f7e4710b9f4
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-01 10:36:56 +0530

    [Divyansh Garg] ADD: Calculating Integer operations.

--- IntegerOperations.java
+++ IntegerOperations.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
-
 public class IntegerOperations {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the first number (a): ");
