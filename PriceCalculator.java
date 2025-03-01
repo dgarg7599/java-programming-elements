4cee0a83657a9d34e6093ab061d017d0a5f719e0
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-01 10:25:59 +0530

    [Divyansh Garg] ADD: Calculating the total purchase price.

---
+++ PriceCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PurchasePriceCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the unit price (INR): ");
+        double unitPrice = input.nextDouble();
+        System.out.print("Enter the quantity to be bought: ");
+        int quantity = input.nextInt();
+        // Calculate total purchase price
+        double totalPrice = unitPrice * quantity;
+        // Display the result
+        System.out.println("The total purchase price is INR " + totalPrice + " if the quantity is " + quantity + " and the unit price is INR " + unitPrice);
+    }
+}
